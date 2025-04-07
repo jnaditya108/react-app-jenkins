cd780a4b280d1878b76989affc5e92061ae4a46b
jnaditya108 <118625801+jnaditya108@users.noreply.github.com>
2025-04-07 10:39:38 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,5 +7,5 @@ variable "location" {
 variable "app_service_plan_sku" {
   description = "The pricing SKU for the Service Plan"
   type        = string
-  default     = "P1v2"  # Adjust based on your needs (e.g., B1, S1, P1v2)
+  default     = "B1"
 }
